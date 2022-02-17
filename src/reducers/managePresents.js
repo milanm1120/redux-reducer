a65fade1ff34069e86ca5e8718187b2a277d64ab
Milan <milanm1120@gmail.com>
2022-02-17 00:09:12 -0500

    commit tree aab6bc59e95f40a0863df9cd08af5e256e21c64d

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,22 @@
-export function managePresents(state, action){
+export function managePresents(state = {managePresents: 0}, action) {
+
+    switch(action.type) {
+        case "INCREASE":
+            return Object.assign({}, state, {
+                numberOfPresents: state.numberOfPresents + 1
+              });
+        default:
+            return state
+    }
 }
+
+
+
+// function managePresents(state = {numberOfPresents: 0}, action) {
+//   switch(action.type) {
+//       case "INCREASE":
+//         return {count: state.count + 1};
+//       default:
+//         return state;
+//   }
+// }
